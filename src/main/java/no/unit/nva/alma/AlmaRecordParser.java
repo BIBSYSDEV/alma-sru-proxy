d11507c6c88762d81702f9a54d7a31a9c40c7efd
gg <gg@unit.no>
2020-02-12 14:12:31 +0100

    code improvments for PMD and jacoco

--- src/main/java/no/unit/nva/alma/AlmaRecordParser.java
+++ src/main/java/no/unit/nva/alma/AlmaRecordParser.java
@@ -1,7 +1,8 @@
 package no.unit.nva.alma;
 
 import com.google.common.io.CharStreams;
-import org.marc4j.MarcStreamReader;
+import org.apache.commons.lang3.StringUtils;
+import org.marc4j.MarcXmlReader;
 import org.marc4j.marc.DataField;
 import org.marc4j.marc.Record;
 import org.marc4j.marc.Subfield;
@@ -12,6 +13,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Scanner;
 
 public class AlmaRecordParser {
 
@@ -22,37 +24,74 @@ public class AlmaRecordParser {
     public static final String EMPTY_TITLE = "EMPTY TITLE";
     public static final String EMPTY_JSON = "{}";
     public static final String EMPTY_STRING = "";
+    public static final String DELIMITER_A = "\\A";
+    public static final String RECORDS_START_TAG = "<records>";
+    public static final String RECORDS_END_TAG = "</records>";
+    public static final String DESIRED_COLLECTION_START_TAG = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<collection xmlns=\"http://www.loc.gov/MARC21/slim\">";
+    public static final String COLLECTION_END_TAG = "</collection>";
+    public static final String BLANK = " ";
+    public static final String NUMBER_OF_RECORDS_IS_ZERO = "<numberOfRecords>0</numberOfRecords>";
+    public static final String MALFORMED_XML_IN_SRU_RESPONSE = "Malformed xml in SRU Response.";
 
 
     /**
      * Parses a SRU-response to extract the title of an marcxml-record.
+     *
      * @param inputStreamReader SRU-response
      * @return simple json with <code>title</code>
      * @throws IOException some stream reading went south
      */
+    @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
     public String extractPublicationTitle(InputStreamReader inputStreamReader) throws IOException {
         InputStream inputStream =
                 new ByteArrayInputStream(CharStreams.toString(inputStreamReader).getBytes(StandardCharsets.UTF_8));
-        final MarcStreamReader reader = new MarcStreamReader(inputStream, StandardCharsets.UTF_8.name());
+        // since Marc4j is not able to parse sru response schema, we have to work with the default marc4j-xml schema
+        // but therefor we need to replace some tags
+        inputStream = this.prepareSruResponseForMarc4jParsing(inputStream);
+        final MarcXmlReader reader = new MarcXmlReader(inputStream);
         if (reader.hasNext()) {
             Record record = reader.next();
             DataField datafield = (DataField) record.getVariableField(MARC_DATAFIELD_245);
+            StringBuilder title = new StringBuilder(EMPTY_TITLE);
             List<Subfield> subfields = datafield.getSubfields();
+            String valueB = null;
             for (Subfield subfield : subfields) {
                 char code = subfield.getCode();
-                String title = EMPTY_STRING;
-                String valueB;
                 if (code == MARC_SUBFIELD_A) {
-                    title = subfield.getData();
+                    title = new StringBuilder(subfield.getData());
                 }
                 if (code == MARC_SUBFIELD_B) {
                     valueB = subfield.getData();
-                    title += valueB;
                 }
-                return String.format(TITLE_AS_JSON, title);
             }
-            return String.format(TITLE_AS_JSON, EMPTY_TITLE);
+            if (StringUtils.isNotEmpty(valueB)) {
+                title.append(BLANK).append(valueB);
+            }
+            return String.format(TITLE_AS_JSON, title.toString());
         }
         return EMPTY_JSON;
+    }
+
+    private InputStream prepareSruResponseForMarc4jParsing(InputStream inputStream) throws IOException {
+        String text;
+        StringBuilder parsedText = new StringBuilder();
+        try (Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8.name())) {
+            text = scanner.useDelimiter(DELIMITER_A).next();
+        }
+        if (text.contains(NUMBER_OF_RECORDS_IS_ZERO)) {
+            parsedText.append(DESIRED_COLLECTION_START_TAG);
+        } else if (text.contains(RECORDS_START_TAG) && text.contains(RECORDS_END_TAG)) {
+            final int startIndex = text.indexOf(RECORDS_START_TAG);
+            final int endIndex = text.indexOf(RECORDS_END_TAG);
+            text = text.substring(startIndex, endIndex);
+            text = text.replace(RECORDS_START_TAG, DESIRED_COLLECTION_START_TAG);
+            parsedText.append(text);
+        } else {
+            throw new IOException(MALFORMED_XML_IN_SRU_RESPONSE);
+        }
+        parsedText.append(COLLECTION_END_TAG);
+        System.out.println("SRU records:" + parsedText.toString());
+        return new ByteArrayInputStream(parsedText.toString().getBytes());
     }
 }
---
+++ src/test/java/no/unit/nva/alma/AlmaRecordParserTest.java
@@ -0,0 +1,71 @@
+package no.unit.nva.alma;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class AlmaRecordParserTest {
+
+
+    public static final String SRU_RESPONSE_2_HITS = "/SRU_response_2_hits.xml";
+    public static final String SRU_RESPONSE_WITH_SUBTITLE = "/SRU_response_with_subtitle.xml";
+    public static final String SRU_RESPONSE_END_TRUNCATED = "/SRU_response_truncated.xml";
+    public static final String SRU_RESPONSE_START_TRUNCATED = "/SRU_response_top_truncated.xml";
+    public static final String SRU_RESPONSE_ZERO_HITS = "/SRU_zero_hits.xml";
+    public static final String EXPECTED_TITLE = "Bedriftsintern telekommunikasjon";
+    public static final String EXPECTED_SUBTITLE =
+            "Emotions and legal judgements : normative issues and empirical findings";
+
+
+    @Test
+    public void testExtractPublicationTitle() throws IOException {
+        InputStream stream = AlmaRecordParserTest.class.getResourceAsStream(SRU_RESPONSE_2_HITS);
+        InputStreamReader inputStreamReader = new InputStreamReader(stream);
+        AlmaRecordParser almaRecordParser = new AlmaRecordParser();
+        String json = almaRecordParser.extractPublicationTitle(inputStreamReader);
+        assertTrue(json.contains(EXPECTED_TITLE));
+    }
+
+
+    @Test
+    public void testExtractPublicationSubtitle() throws IOException {
+        InputStream stream = AlmaRecordParserTest.class.getResourceAsStream(SRU_RESPONSE_WITH_SUBTITLE);
+        InputStreamReader inputStreamReader = new InputStreamReader(stream);
+        AlmaRecordParser almaRecordParser = new AlmaRecordParser();
+        String json = almaRecordParser.extractPublicationTitle(inputStreamReader);
+        assertTrue(json.contains(EXPECTED_SUBTITLE));
+    }
+
+    @Test
+    public void testExtractPublicationTitle_FromZeroHits() throws IOException {
+        InputStream stream = AlmaRecordParserTest.class.getResourceAsStream(SRU_RESPONSE_ZERO_HITS);
+        InputStreamReader inputStreamReader = new InputStreamReader(stream);
+        AlmaRecordParser almaRecordParser = new AlmaRecordParser();
+        String json = almaRecordParser.extractPublicationTitle(inputStreamReader);
+        assertEquals(AlmaRecordParser.EMPTY_JSON, json);
+    }
+
+    @Test(expected = IOException.class)
+    public void testExtractPublicationTitle_MalformedSruResponseOnEnd() throws IOException {
+        InputStream stream = AlmaRecordParserTest.class.getResourceAsStream(SRU_RESPONSE_END_TRUNCATED);
+        InputStreamReader inputStreamReader = new InputStreamReader(stream);
+        AlmaRecordParser almaRecordParser = new AlmaRecordParser();
+        almaRecordParser.extractPublicationTitle(inputStreamReader);
+        fail("Since the SRU response is truncated and thus unreadable, we expect an exception to be thrown");
+    }
+
+    @Test(expected = IOException.class)
+    public void testExtractPublicationTitle_MalformedSruResponseOnStart() throws IOException {
+        InputStream stream = AlmaRecordParserTest.class.getResourceAsStream(SRU_RESPONSE_START_TRUNCATED);
+        InputStreamReader inputStreamReader = new InputStreamReader(stream);
+        AlmaRecordParser almaRecordParser = new AlmaRecordParser();
+        almaRecordParser.extractPublicationTitle(inputStreamReader);
+        fail("Since the SRU response is truncated and thus unreadable, we expect an exception to be thrown");
+    }
+}
