7e958b9bea3d9d8174c7eb4b9b35264c2ac5a697
Tor Arne Torgersen <6398417+torarnet@users.noreply.github.com>
2025-05-27 10:36:10 +0200

    ts-43: update checkstyle version

--- src/main/java/no/unit/alma/DebugUtils.java
+++ src/main/java/no/unit/alma/DebugUtils.java
@@ -18,7 +18,7 @@ public class DebugUtils {
     }
 
     /**
-     * From org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace
+     * From org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace.
      * in org.apache.commons:commons-lang3:3.9
      */
     public static String getStackTrace(final Throwable throwable) {
